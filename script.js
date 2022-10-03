f049a26974eb6e14bdd1ccb59410aaeac36af805
Cylecq <leclercq.cyril06@gmail.com>
2022-10-03 20:56:16 +0200

    quickSave trying random question pull

--- script.js
+++ script.js
@@ -8,4 +8,59 @@
 //   p.innerHTML = "hello world";
 //   document.querySelector("main").appendChild(p);
 // });
-console.log('coucou');
+
+let score = 0;
+
+let questions = [
+  
+  q1 = {
+  
+    category: 'Geography',
+    type: 'multiple',
+    difficulty: 'medium',
+    question: 'On a London Underground map, what colour is the Circle Line?',
+    correct_answer: 'Yellow',
+    incorrect_answers: ["Red", "Blue", "Green"]
+  },
+
+  q2 = {
+    category: 'Geography',
+    type: 'multiple',
+    difficulty: 'medium',
+    question: "hich of these countries is &quot;doubly landlocked&quot; (surrounded entirely by one or more landlocked countries)?",
+    correct_answer: "Uzbekistan",
+    incorrect_answers: ["Switzerland", "Bolivia", "Ethiopia"]
+  },
+
+  q3 = {
+    category: "History",
+    type: "multiple",
+    difficulty: "medium",
+    question: "Which of these countries was sea charted in 1500 by the Portuguese maritime explorations?",
+    correct_answer: "Brazil",
+    incorrect_answers: ["India", "Mozambique", "Madagascar"]
+  },
+
+];
+
+function newQuestion(){
+  let questionIndex = Math.floor(Math.random) * questions.length;
+  console.log(questionIndex);
+  console.log(parseInt(questionIndex));
+};
+
+newQuestion();
+
+function startQuestion(){
+
+};
+
+
+function selectAnswer(){
+
+};
+
+
+function NextQuestion(){
+
+};
