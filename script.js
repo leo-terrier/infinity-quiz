202f7f10e34b09530bb8fc8e2ab511d408600f91
Cylecq <leclercq.cyril06@gmail.com>
2022-10-03 20:18:56 +0200

    clean HTML ready to go JS

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Exemple pour faire apparaitre des éléments dans le DOM
 
-document.querySelector(".logo-container").addEventListener("click", function () {
-  const p = document.createElement("p");
-  p.addEventListener("click", function () {
-    p.style.display = "none";
-  });
-  p.innerHTML = "hello world";
-  document.querySelector("main").appendChild(p);
-});
+// document.querySelector(".logo-container").addEventListener("click", function () {
+//   const p = document.createElement("p");
+//   p.addEventListener("click", function () {
+//     p.style.display = "none";
+//   });
+//   p.innerHTML = "hello world";
+//   document.querySelector("main").appendChild(p);
+// });
+console.log('coucou');
